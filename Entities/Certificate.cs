cc9c557ba66d2a915602e174f432e0938eeba9c3
MatheusKoszudikass <matheusprgc@gmail.com>
2025-02-28 19:10:15 -0300

    feat: Envio correto da informações do cliente.

---
+++ Entities/Certificate.cs
@@ -0,0 +1,13 @@
+using System.Security.Cryptography.X509Certificates;
+
+namespace ClientBlockchain.Entity;
+
+public class Certificate
+{
+    private readonly X509Certificate2 _clientCertificate;
+
+    public Certificate()
+    {
+        
+    }
+}
---
+++ Entities/Listener.cs
@@ -0,0 +1,78 @@
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using ClientBlockChain.InstructionsSocket;
+
+namespace ClientBlockChain.Entity
+{
+    public class Listener(uint port)
+    {
+        Socket WorkSocket = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private static readonly IPHostEntry Domain = Dns.GetHostEntryAsync("monerokoszudikas.duckdns.org").Result;
+        public static readonly IPAddress Ip = Domain.AddressList[0];
+        public bool Listening { get; private set; }
+        public uint Port { get; private set; } = port;
+
+        public async Task Start()
+        {
+            if (this.Listening) return;
+
+            this.Listening = true;
+
+            await ConnectWithRetry();
+        }
+
+        private async Task ConnectWithRetry()
+        {
+            while (true)
+            {
+                try
+                {
+                    if (!this.WorkSocket.Connected)
+                    {
+                        Console.WriteLine($"Tentando conectar ao servidor... {Ip}");
+                        await this.WorkSocket.ConnectAsync(Ip, checked((int)Port));
+
+                        // var completedTask = await Task.WhenAny(task, Task.Delay(5000));
+
+                        // if (completedTask.IsFaulted) throw new SocketException();
+
+
+                        Console.WriteLine("Conectado ao servidor!");
+                        break;
+
+                    }
+                }
+                catch (SocketException)
+                {
+                    Console.WriteLine("Erro de conexão. Tentando novamente em 5 segundos...");
+                    //this.WorkSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    await Task.Delay(5000);
+                }
+            }
+        }
+
+        public Socket GetSocket()
+        {
+            return this.WorkSocket;
+        }
+
+        public void Reconnect()
+        {
+            this.Listening = false;
+        }
+        public void Stop()
+        {
+            if (!this.Listening) return;
+
+            this.WorkSocket.Close();
+
+            this.Listening = false;
+        }
+
+        protected virtual void OnStatusClientConnected(Socket e) => StatusClientConnected?.Invoke(this,e);
+
+        public event EventHandler<Socket>? StatusClientConnected;
+    }
+
+}
